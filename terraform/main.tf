e7b6cacbce9b31568d94a5944f699f30c5f28828
Kerby Ferris <kerby@friendbuy.com>
2018-10-13 00:16:29 -0700

    terraform s3

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile = "${var.AWS_PROFILE}"
+  region  = "${var.AWS_REGION}"
+}
+
+module "kerbyferris_live" {
+  source      = "./s3_website"
+  bucket_name = "kerbyferris.com"
+  aws_region  = "${var.AWS_REGION}"
+  # root_dns  = "${var.root_dns}"
+  # zone_id   = "${module.route53.zone_id}"
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,8 @@
+variable "AWS_PROFILE" {
+  default = "kerbyferris"
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
