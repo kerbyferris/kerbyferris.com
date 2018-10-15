285c19340a3c39d892ebb088cd99e2e780e3dda1
Kerby Ferris <kerby@friendbuy.com>
2018-10-14 17:49:30 -0700

    fix links

--- src/kerbyferris/views/sounds.cljs
+++ src/kerbyferris/views/sounds.cljs
@@ -3,15 +3,18 @@
 (defn sounds[]
   [:div#sounds 
     [:p.basic_box "Production Samples:"]
-    [:a {:href "https://soundcloud.com/pawprincemusic/wild-horses/s-vEPxZ" :class "sc-player"}
-     "Wild Horses"]
-    [:a {:href "https://soundcloud.com/pawprincemusic/tigersquare/s-ksj0I" :class "sc-player"}
-     "Tiger Square"]
-    [:a {:href "https://soundcloud.com/pawprincemusic/virginia/s-NzFkw" :class "sc-player"}
-     "Virginia"]
-    [:a {:href "https://soundcloud.com/pawprincemusic/face-challenges-edit/s-028zA" :class "sc-player"}
+    [:a {:href "https://soundcloud.com/kerbyferris/kerby-ferris-ojai" :class "sc-player"}
+     "Ojai"]
+    [:a {:href "https://soundcloud.com/kerbyferris/face-challenges" :class "sc-player"}
      "FAce Challenges"]
-    [:a {:href "https://soundcloud.com/pawprincemusic/family-of-light/s-hpOiH" :class "sc-player"}
+    [:a {:href "https://soundcloud.com/kerbyferris/its-always-more-beautiful-to" :class "sc-player"}
+     "It's Always More Beautiful to Say Hello"]
+    [:a {:href "https://soundcloud.com/lavender-mirror/family-of-light-2" :class "sc-player"}
      "Family of Light"]
-    [:a {:href "http://soundcloud.com/pawprincemusic/wander-through-the-time-of/s-HgGYE" :class "sc-player"}
-     "Wander Through the Time of Hearts"]])
+    [:a {:href "https://soundcloud.com/lavender-mirror/virginia" :class "sc-player"}
+     "Virginia"]
+    [:a {:href "https://soundcloud.com/lavender-mirror/third-dimensional" :class "sc-player"}
+     "Third Dimensional"]
+    [:a {:href "https://soundcloud.com/kerbyferris/wild-horses/s-vEPxZ" :class "sc-player"}
+     "Wild Horses"]])
+
