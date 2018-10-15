8814b2298084f2376575109c7f6cdec3945c3cc5
Kerby Ferris <kerby@friendbuy.com>
2018-10-15 01:53:44 -0700

    pause on https

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,82 +26,82 @@ resource "aws_route53_zone" "zone" {
   comment = "kerbyferris.com root DNS"
 }
 
-# resource "aws_route53_record" "alias" {
-#   zone_id = "${aws_route53_zone.zone.zone_id}"
-#   name    = "kerbyferris.com"
-#   type    = "A"
-
-#   alias = {
-#     name                   = "${aws_s3_bucket.site.website_domain}"
-#     zone_id                = "${aws_s3_bucket.site.hosted_zone_id}"
-#     evaluate_target_health = false
-#   }
-# }
-
-data "aws_acm_certificate" "cert" {
-  domain      = "kerbyferris.com"
-  statuses    = ["ISSUED"]
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
-}
-
-resource "aws_cloudfront_distribution" "distribution" {
-  origin {
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
-    }
-
-    domain_name = "${aws_s3_bucket.site.bucket_regional_domain_name}"
-    origin_id   = "kerbyferris.com"
-  }
-
-  enabled             = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-
-    target_origin_id = "kerbyferris.com"
-    min_ttl          = 0
-    default_ttl      = 86400
-    max_ttl          = 31536000
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  aliases = ["kerbyferris.com"]
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
-    ssl_support_method  = "sni-only"
-  }
-}
-
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {}
-
-resource "aws_route53_record" "site_alias" {
-  name    = "kerbyferris.com"
+resource "aws_route53_record" "alias" {
   zone_id = "${aws_route53_zone.zone.zone_id}"
+  name    = "kerbyferris.com"
   type    = "A"
 
   alias = {
-    name                   = "${aws_cloudfront_distribution.distribution.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.distribution.hosted_zone_id}"
+    name                   = "${aws_s3_bucket.site.website_domain}"
+    zone_id                = "${aws_s3_bucket.site.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
+
+# data "aws_acm_certificate" "cert" {
+#   domain      = "kerbyferris.com"
+#   statuses    = ["ISSUED"]
+#   types       = ["AMAZON_ISSUED"]
+#   most_recent = true
+# }
+
+# resource "aws_cloudfront_distribution" "distribution" {
+#   origin {
+#     s3_origin_config {
+#       origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+#     }
+
+#     domain_name = "${aws_s3_bucket.site.bucket_regional_domain_name}"
+#     origin_id   = "kerbyferris.com"
+#   }
+
+#   enabled             = true
+#   default_root_object = "index.html"
+
+#   default_cache_behavior {
+#     viewer_protocol_policy = "redirect-to-https"
+#     compress               = true
+#     allowed_methods        = ["GET", "HEAD"]
+#     cached_methods         = ["GET", "HEAD"]
+
+#     target_origin_id = "kerbyferris.com"
+#     min_ttl          = 0
+#     default_ttl      = 86400
+#     max_ttl          = 31536000
+
+#     forwarded_values {
+#       query_string = false
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+#   }
+
+#   aliases = ["kerbyferris.com"]
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
+
+#   viewer_certificate {
+#     acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
+#     ssl_support_method  = "sni-only"
+#   }
+# }
+
+# resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {}
+
+# resource "aws_route53_record" "site_alias" {
+#   name    = "kerbyferris.com"
+#   zone_id = "${aws_route53_zone.zone.zone_id}"
+#   type    = "A"
+
+#   alias = {
+#     name                   = "${aws_cloudfront_distribution.distribution.domain_name}"
+#     zone_id                = "${aws_cloudfront_distribution.distribution.hosted_zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
